2af622416bbe53b6b8d69f7990fce3489eb11c6e
Fantasystar94 <aniseed_3@naver.com>
2022-08-30 02:02:25 +0900

    블로그 제작2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  let post = '리액트로 블로그 만드는 프로젝트';
+  let [blogTitle,b] = useState('남자 코트 추천');
+
   return (
     <div className="App">
       <div className='header'>
          <h4>ReactBlog</h4>
       </div>
-      <h5>Title</h5>
+      <h5>{post}</h5>
+      <div className='list'>
+        <dl>
+          <dt>{blogTitle}</dt>
+          <dd>8월 30일 발행</dd>
+        </dl>
+        <dl>
+          <dt>글 제목</dt>
+          <dd>8월 30일 발행</dd>
+        </dl>
+        <dl>
+          <dt>글 제목</dt>
+          <dd>8월 30일 발행</dd>
+        </dl>
+      </div>
     </div>
   );
 }
